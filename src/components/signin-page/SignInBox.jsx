57537a18d175c1445ced651d4966e8e908161a79
ledanhtrong <trongldse151359@fpt.edu.vn>
2021-09-30 13:06:02 +0700

    fix bug logo disappear

--- src/components/signin-page/SignInBox.jsx
+++ src/components/signin-page/SignInBox.jsx
@@ -1,9 +1,11 @@
 import SignInGoogle from "./SignInGoogle";
 import SignInLocalForm from "./SignInLocalForm";
+import Logo from "./Logo";
 
 function SignInBox() {
   return (
     <div className="sign-in">
+      <Logo />
       <SignInLocalForm />
       <SignInGoogle />
     </div>
